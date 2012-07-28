9ccf541e3d4d600f987fff28831d8092adb509d5
Craig Michael Thompson <craig@ws1.(none)>
2012-07-28 16:57:25 +0000

    Initial commit

--- app.js
+++ app.js
@@ -4,30 +4,68 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+	, routes = require('./routes')
+	, http = require('http')
+	, path = require('path')
+	, date = require('date').date
+	, nsh = require('node-syntaxhighlighter')
+	, markdown = require('markdown').markdown;
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.set('port', process.env.PORT || 80);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.set('view options', {
+		layout: 'layouts/default'
+	});
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
+app.get('/about', routes.about);
+app.get('/demos', routes.demos);
+app.get('/download', routes.download);
+app.get('/donate', routes.donate);
+app.get('/converter', routes.converter);
+app.get('/docs/:page?', routes.docs);
+
+// Setup view helpers
+app.locals({
+	// URL helpers
+	WEB_DIR: '/.',
+	COMMON_DIR: 'http://media1.juggledesign.com/common',
+	CSS_DIR: 'http://media1.juggledesign.com/qtip2/css',
+	JS_DIR: 'http://media1.juggledesign.com/qtip2/js',
+	IMAGES_DIR: 'http://media1.juggledesign.com/qtip2/images',
+	GIT_URI: 'http://github.com/Craga89/qTip2/',
+
+	// qTip packages
+	QTIP_JS: 'http://craigsworks.com/projects/qtip2/packages/nightly/jquery.qtip.min.js',
+	QTIP_CSS: 'http://craigsworks.com/projects/qtip2/packages/nightly/jquery.qtip.min.css',
+
+	// Helpers
+	date: date,
+	markdown: function(str) {
+		return markdown.toHTML(str);
+	},
+	highlight: function(lang, code) {
+		return nsh.highlight(
+			code, nsh.getLanguage(lang)
+		);
+	}
+})
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+	console.log("Express server listening on port " + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,86 @@
+var fs = require('fs')
+	, markdown = require('markdown').markdown;
 
 /*
- * GET home page.
+ * Home page
  */
+exports.index = function(req, res) {
+	res.render('index', {
+		title: [],
+		page: 'home'
+	});
+}
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+/*
+ * About
+ */
+exports.about = function(req, res) {
+	res.render('about', {
+		title: ['About'],
+		page: 'about'
+	})
+}
+
+/*
+ * Demos
+ */
+exports.demos = function(req, res) {
+	res.render('demos', {
+		title: ['Demos'],
+		page: 'demos'
+	});
+}
+
+/*
+ * Download
+ */
+exports.download = function(req, res) {
+	res.render('download', {
+		title: ['Download'],
+		page: 'download'
+	});
+}
+
+/*
+ * Donate
+ */
+exports.donate = function(req, res) {
+	res.render('donate', {
+		title: ['Donate'],
+		page: 'donate'
+	});
+}
+
+/*
+ * Converter
+ */
+exports.converter = function(req, res) {
+	res.render('converter', {
+		title: ['v1.0 Code Converter'],
+		page: 'converter'
+	});
+}
+
+
+/*
+ * Documentation
+ */
+exports.docs = function(req, res, next) {
+	var page = req.params[1] || '',
+		file = 'docs/' + (page || 'gettingstarted') + '.md';
+
+	// Open file if it exists
+	if(fs.exists(file)) {
+		content = fs.readFileSync(file);
+		content = markdown.toHTML(content);
+	}
+
+	// Otherwise throw an error and continue
+	else { return next(new Error('Unable to find documentation ' + file)); }
+
+	res.render('docs/'+page, {
+		title: ['Documentation', page.substr(0,1).toUpperCase() + page.substr(1) ],
+		page: 'docs',
+		content: content
+	});
+}
