a526fcd694584c162ac9c8f2beef3646dcacab29
Craig Michael Thompson <craig@craigsworks.com>
2013-03-14 22:20:04 +0000

    Add legeldn activating scroll functionality. Taken from expressjs.com

--- public/javascripts/docs.js
+++ public/javascripts/docs.js
@@ -33,6 +33,50 @@ $('a[name*="."]').each(function() {
 	}
 });
 
+// Highlight different legend links on scroll
+// Adjusted from expressjs.com's source: http://expressjs.com/app.js
+var win = $(window),
+	winHeight = win.height(),
+	headings;
+
+headings = $('a', legend).map(function(i) {
+	var anchor = $('a[name="'+this.hash.substr(1)+'"]'),
+		parent = anchor.parents('.category, .section');
+
+	return !anchor.length ? null : {
+		elem: $(this),
+		top: (parent.length ? parent : anchor).offset().top
+	}
+}).toArray();
+
+function closest() {
+	var top = win.scrollTop(),
+		i = headings.length,
+		h;
+
+	while(i--) {
+		h = headings[i];
+		if(h.top > top && h.top < top + winHeight / 2){
+			return h;
+		} 
+	}
+}
+
+$(document).scroll(function() {
+	// Don't do it when we're animating
+	if( $('body').is(':animated') ) { return; }
+
+	var h = closest(), a;
+	if(!h) return;
+
+	$('ul, a', legend).removeClass('active');
+	h.elem.parents('ul.submenu').andSelf()
+		.add( h.elem.next('ul.submenu') )
+		.addClass('active');
+});
+$(function() { $(document).scroll(); });
+
+
 // On anchor change... update the legend
 $(window).hashchange(function() {
 	var name = document.location.hash.substr(1),
@@ -41,10 +85,9 @@ $(window).hashchange(function() {
 	var link = $('a[href="#'+parts[0]+'"]', legend);
 	if(!link.length) { return; }
 
-	$('.submenu', legend).removeClass('active');
-	link.next('.submenu').addClass('active');
-
-	$('ul a', legend).removeClass('active');
-	$('a[href="#'+name+'"]', legend).addClass('active');
+	$('ul, a', legend).removeClass('active');
+	$('a[href="#'+name+'"]', legend).add(
+		link.next('.submenu')
+	).addClass('active');
 })
 .hashchange();
